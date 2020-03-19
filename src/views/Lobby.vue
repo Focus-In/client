60b2b1644529acee1749aab1d23a2a83403dcb9d
bhaktitud <myforework@live.com>
2020-03-19 18:37:14 +0700

    home > lobby > playgame

--- src/views/Lobby.vue
+++ src/views/Lobby.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="about">
-    {{players}}
+    <div class="overflow-auto">
+       {{players}}
+    </div>
+    <div>
+       <router-link to="/playgame">
+        <button>Start</button>
+       </router-link>
+    </div>
   </div>
 </template>
 
