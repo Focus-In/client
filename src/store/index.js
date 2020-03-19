7e4cc3a1c81c3717f3ef44d6d474d613ae9f3177
bhaktitud <myforework@live.com>
2020-03-19 21:40:16 +0700

    PLAYGROUND 70%
    - validate user's answer
    - working on scoring
    - img url needs to be store cloud

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,19 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    players: []
+    players: [],
+    questionList: [
+      { id: 1, imgUrl: 'https://hackernoon.com/hn-images/1*GuuDpW8KefKQDcUW5NtgGA.png', answer: 'B' },
+      { id: 2, imgUrl: 'src/assets/soal/soal3.svg', answer: 'A' },
+      { id: 3, imgUrl: 'src/assets/soal/soal3.svg', answer: 'B' },
+      { id: 4, imgUrl: 'src/assets/soal/soal3.svg', answer: 'B' },
+      { id: 5, imgUrl: 'src/assets/soal/soal3.svg', answer: 'B' },
+      { id: 6, imgUrl: 'src/assets/soal/soal3.svg', answer: 'B' },
+      { id: 7, imgUrl: 'src/assets/soal/soal3.svg', answer: 'A' },
+      { id: 8, imgUrl: 'src/assets/soal/soal3.svg', answer: 'A' },
+      { id: 9, imgUrl: 'src/assets/soal/soal3.svg', answer: 'A' },
+      { id: 10, imgUrl: 'src/assets/soal/soal3.svg', answer: 'A' }
+    ]
   },
   mutations: {
     SET_PLAYERS (state, payload) {
@@ -16,7 +28,18 @@ export default new Vuex.Store({
     }
   },
   actions: {
-
+    checkAnswer ({ dispatch, commit, state }, { answer, questionNumber }) {
+      const questions = state.questionList
+      for (let index = 0; index < questions.length; index++) {
+        if (questions[index].id === questionNumber) {
+          if (questions[index].answer === answer) {
+            console.log('you got a point')
+          } else {
+            console.log('you gotta need to be focus!')
+          }
+        }
+      }
+    }
   },
   modules: {
   }
