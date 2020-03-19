934db278bf281288b034d975e27b3735d4abf788
hilmimuharromi <hilmimuharrom@Gmail.com>
2020-03-19 15:39:51 +0700

    playgame-page

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Lobby from '../views/Lobby.vue'
+import PlayGame from '../views/PlayGame.vue'
 
 Vue.use(VueRouter)
 
@@ -15,6 +16,11 @@ const routes = [
     path: '/lobby',
     name: 'lobby',
     component: Lobby
+  },
+  {
+    path: '/playgame',
+    name: 'PlayGame',
+    component: PlayGame
   }
 ]
 
---
+++ src/views/PlayGame.vue
@@ -0,0 +1,47 @@
+<template>
+<div class="container">
+    <div class="play-board">
+        <div class="play-board-header">
+            <div class="score-user">
+                Your Score: 10
+            </div>
+            <div class="logo">
+                <h2>Focus In</h2>
+            </div>
+            <div class="score-user">
+                Another User Score: 10
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+.play-board{
+    display: flex;
+    justify-content: center;
+    margin-top: 10vh;
+    background-color: beige;
+}
+
+.play-board-header{
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    margin: 5px;
+}
+
+.play-board-header div{
+    background-color: aquamarine;
+    margin: 5px;
+    padding: 5px;
+}
+
+</style>
