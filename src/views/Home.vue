999e48d2c747390de73279bfb77f3271bf9fafad
bhaktitud <myforework@live.com>
2020-03-19 16:02:57 +0700

    HOME VERSI BHAKTI
    - audio added
    - client connected to server by using socket
    - still develop the interaction

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,36 +1,44 @@
 <template>
   <div class="home">
-    <form @submit.prevent='inputUser'>
-      <input type="text" class="form-control" v-model="username">
-      <button>Submit</button>
+    <form @submit.prevent='inputUser' class="form-group">
+      <div class="container">
+         <input type="text" class="form-control" v-model="username">
+         <button type="submit">Submit</button>
+      </div>
     </form>
   </div>
 </template>
 
 <script>
-// import io from 'socket.io-client'
+import io from 'socket.io-client'
 // const socket = io.connect('http://localhost:3000')
 
 export default {
   name: 'Home',
   data () {
     return {
-      username: ''
+      username: '',
+      socket: {}
     }
+  },
+  methods: {
+    inputUser () {
+      this.$store.commit('SET_PLAYERS', this.username)
+      this.socket.emit('addPlayer', {
+        username: this.username
+      })
+    }
+  },
+  created () {
+    this.socket = io.connect('http://localhost:3000')
+    const audio = new Audio('https://soundimage.org/wp-content/uploads/2014/08/Netherplace.mp3')
+    audio.play()
+  },
+  mounted () {
+    this.socket.on('playerAdded', payload => {
+      this.$router.push('/lobby')
+    })
   }
-  // methods: {
-  //   inputUser () {
-  //     this.$store.commit('SET_PLAYERS', this.username)
-  //     socket.emit('addPlayer', {
-  //       username: this.username
-  //     })
-  //   }
-  // },
-  // created () {
-  //   socket.on('playerAdded', payload => {
-  //     this.$router.push('/lobby')
-  //   })
-  // }
 
 }
 </script>
